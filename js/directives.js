555030f1b25330f176f69797a0bea5cc8e3e795f
Anthony Marshall <anthony.marshall@wearesuburb.com>
2014-06-09 21:57:09 +0100

    fix responsive

--- js/directives.js
+++ js/directives.js
@@ -14,4 +14,29 @@ angular.module('AppName.directives', [])
 
         }
     }
-	});
+	})
+
+	.directive('fwcMasonaryresize', ['$window', function($window) {
+	    return {
+	        link: function(scope, elem, attrs) {
+	            scope.onResize = function() {
+	               var resizeFactor = Math.ceil($window.innerWidth / 160);
+	               if(resizeFactor > 6) resizeFactor = 6;
+
+	               var newItemWidth = Math.floor($window.innerWidth / resizeFactor);
+	               var styleNode = document.createElement('style');
+					styleNode.setAttribute("type", "text/css");
+					var textNode = document.createTextNode(".feed-item{width: "+newItemWidth+"px !important;}");
+					styleNode.appendChild(textNode);
+					document.getElementsByTagName("head")[0].appendChild(styleNode);
+	            }
+
+	            scope.onResize();
+
+	            angular.element($window).bind('resize', function() {
+	                scope.onResize();
+	            })
+	        }
+	    }
+	}])
+
