a2f341770869e543a1051e99c039f04f4c5e9084
wearesuburb <anthony.marshall@wearesuburb.com>
2014-06-13 10:02:29 +0100

    compensate for scroll bars

--- js/directives.js
+++ js/directives.js
@@ -44,14 +44,16 @@ angular.module('AppName.directives', [])
 	})
 
 
-	.directive('fwcMasonaryresize', ['$window', function($window) {
+	.directive('fwcMasonaryresize', ['$window', '$document', function($window, $document) {
 	    return {
 	        link: function(scope, elem, attrs) {
 	            scope.onResize = function() {
-	               	var resizeFactor = Math.ceil($window.innerWidth / 160);
+	            	var scrollbarWidth = 15;
+	            	var docWidth = $window.innerWidth - scrollbarWidth;
+	               	var resizeFactor = Math.ceil(docWidth / 160);
 	               	if(resizeFactor > 6) resizeFactor = 6;
 
-	               	var newItemWidth = Math.floor($window.innerWidth / resizeFactor);
+	               	var newItemWidth = Math.floor(docWidth / resizeFactor);
 	               	var styleNode = document.createElement('style');
 					styleNode.setAttribute("type", "text/css");
 					var textNode = document.createTextNode(".feed-item{width: "+newItemWidth+"px !important;} .feed .feed-item.selected{margin-left:-"+newItemWidth/2+"px;margin-top:-"+newItemWidth/2+"px}");
